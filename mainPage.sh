928d22ac9f78951cf4e5fe93b6c9664be4380908
NathanEid <nesoeid4@gmail.com>
2022-12-01 23:56:11 +0200

    MainPage.sh

---
+++ mainPage.sh
@@ -0,0 +1,16 @@
+echo "Welcome to the MySQL monitor."
+
+select op in "Create Database" "List Database" "Connect to Database" "Drop Database"
+do
+    case $op in
+        "Create Database" )
+        ;;
+        "List Database" )
+        ;;
+        "Connect to Database" )
+        ;;
+        "Drop Database" )
+        ;;
+    esac
+done
+
